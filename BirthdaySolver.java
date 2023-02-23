958ad630202aa94ba9a1bd612faa89fb268b0fcc
Matt <mjernst31@comcast.net>
2023-02-23 11:13:10 -0500

    v1.1 -> 2/23 -> JavaDoc

--- BirthdaySolver.java
+++ BirthdaySolver.java
@@ -1,27 +1,25 @@
-/*   @Author - Matthew Ernst
-     @Version - 1.1.         */
-
 import java.util.*;
+
+/**
+ * @author Matthew Ernst
+ * @version 1.1
+ */
 class BirthdaySolver {
     static Random rand = new Random();
 
-    /*** Generates random birthdates for a group of people and returns the chance of two people having the same birthdate.*
-     * @return the chance of two people in the group having the same birthdate   */
+    /**
+     * Generates random birthdates for a group of people and returns the chance of two people having the same birthdate.
+     * @return the chance of two people in the group having the same birthdate
+     * */
     public static double generateDates() {
         ArrayList<Integer> groupedCollection = new ArrayList<>();
         boolean run = true;
 
         groupedCollection.add(rand.nextInt(365) + 1);
         while (run) {
-            //Denotes the selected element...
             for (int i = 0; i < groupedCollection.size(); i++) {
-                //Denotes all the elements you are comparing to the selected element...
                 for (int j = 0; j < groupedCollection.size(); j++) {
-
-                    //Aslong as its not comparing the same element and the elements both exist at different indexes...
                     if ((i != j) && (groupedCollection.get(i).equals(groupedCollection.get(j)))) {
-
-                        //Returns the chance
                         return (1.0/groupedCollection.size()) * 100;
                     }
                 }
